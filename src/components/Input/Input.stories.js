6f6273ad05bd8cc6cdbce80ddec2ab8acd609579
tony <antolisah@gmail.com>
2021-07-06 01:51:23 +0300

    renaming and sorting stories

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -8,3 +8,5 @@ export default {
 export const Small = () => <Input size='small' placeholder='small' />
 export const Medium = () => <Input size='medium' placeholder='medium' />
 export const Large = () => <Input size='large' placeholder='large' />
+
+Small.storyName = 'Small Input'
